d526f48d5fd82bbbf325e2859b9ed00a85b14ece
tanialiudkevych <tanialiudkevych@gmail.com>
2016-05-24 18:09:07 +0300

    перевірка числа на паліндром

---
+++ Palindroms.js
@@ -0,0 +1,20 @@
+function palindrome(str) {
+  
+  str = str.toLowerCase();
+  str = str.replace(/[^a-zA-Z0-9]/g,'');
+  var myArray = str.split("");
+  var x = myArray.length;
+  var flag = 0;
+ 
+  for(var i=0; i<x; i++) {
+    if (i<=x/2) {
+      if(myArray[i] == myArray[x-i-1]) {
+          flag = flag + 0;   
+       } else flag = flag + 1;
+    }
+  }
+  if (flag === 0) {
+    return true;
+  } else return false;
+}
+
